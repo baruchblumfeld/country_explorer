7aa6df736dbe609870c16a32a8e5f0c9f9b8a212
Bart <48806421+baruchblumfeld@users.noreply.github.com>
2026-06-16 13:47:00 +0200

    Refactor API calls for country data retrieval

--- script.js
+++ script.js
@@ -34,47 +34,48 @@ const renderError = function (message) {
 };
 
 const getJSON = function (url, errorMessage = 'Something went wrong') {
-  return fetch(url, {
-    headers: {
-      Authorization: `Bearer ${API_KEY}`,
-    },
-  }).then((response) => {
+  return fetch(url).then((response) => {
+    console.log(response);
     if (!response.ok) throw new Error(`${errorMessage} (${response.status})`);
     return response.json();
   });
 };
 
 const getCountryAndNeighbours = function (country) {
-  getJSON(
-    `https://api.restcountries.com/countries/v5?q=${country}`,
-    'Country not found'
-  )
+  getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
     .then((data) => {
+      console.log(data);
       renderCountry(data[0]);
 
-      const neighbour = data[0].borders;
-      if (!neighbour || neighbour.length === 0) {
-        throw new Error('No neighbour found');
-      }
+      //   const neighbour = data[0].borders?.[0];
 
-      // 🔥 KLUCZ: return kończy tego thena
+      //   return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+      // })
+      // .then((response) => response.json())
+      // .then((data) => {
+      //   renderCountry(data[0], 'neighbour');
+      // });
+      ////////
+
+      const neighbour = data[0].borders;
+      if (!('borders' in data[0])) throw new Error('No neighbour found');
       return getJSON(
-        `https://api.restcountries.com/countries/v5?codes=${neighbour.join(',')}`,
-        'Country not found'
+        `https://restcountries.com/v3.1/alpha?codes=${neighbour.join(',')}`,
+        'Country not found',
       );
     })
     .then((resData) => {
       resData.forEach((data) => {
-        renderCountry(data, undefined, undefined, 'neighbour');
+        const languages = Object.values(data.languages);
+        const currencies = Object.values(data.currencies);
+        renderCountry(data, languages, currencies, 'neighbour');
       });
     })
     .catch((err) => {
       console.error(err);
       renderError(`Something went wrong: ${err.message}. Try again!`);
     })
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
-    });
+    .finally(() => (countriesContainer.style.opacity = 1));
 };
 
 btn.addEventListener('click', function () {
